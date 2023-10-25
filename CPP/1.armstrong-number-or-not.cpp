9b81c3467f8fbecf7999dc16371fe8d03b1ff9d2
jack <jack@jack.com>
2023-10-25 15:02:38 +0530

    Fixed Error

--- CPP/1.armstrong-number-or-not.cpp
+++ CPP/1.armstrong-number-or-not.cpp
@@ -1,11 +1,14 @@
 //write a program to check a number is armstrong or not.
+
 #include <iostream>
 using namespace std;
 
-bool isArmstrong(int n);
+bool isArmstrong(long);
+long pow(int, int);
+int numLen(long);
 
 int main(){
-    int n;
+    long n;
 	cout << "Enter Your Number: ";
 	cin >> n;
     if(isArmstrong(n)){
@@ -15,21 +18,41 @@ int main(){
     }
     return 0;
 }
-bool isArmstrong(int n){
-    if(n<10){
+bool isArmstrong(long n){
+    if(n < 1){
+        return 0;
+    }else if(n<10){
 		return 1;
 	}
-	int sum = 0;
-    int n1 = n;
+	long sum = 0;
+    int len = numLen(n);
+    long n1 = n;
     while(n>9){
-        sum += (n%10)*(n%10)*(n%10);
+        sum += pow((n%10),len);
         n /= 10;
     }
-    sum+=n*n*n;
+    sum+=pow(n,len);
     if(sum == n1){
         return 1;
     }else{
         return 0;
 
     }
+}
+
+long pow(int a, int b){
+    long pow = 1;
+    for(int i = 0; i < b; i++){
+        pow *= a;
+    }
+    return pow;
+}
+
+int numLen(long a){
+    int len = 0;
+    while(a>0){
+        len++;
+        a = a/10;
+    }
+    return len;
 }
